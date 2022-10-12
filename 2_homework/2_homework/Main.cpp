173ec4f7fc2106c25fc36491661e4c08d3564ef5
sashatikhonov7 <44275252+sashatikhonov7@users.noreply.github.com>
2022-10-12 19:35:14 +0300

    Add files via upload

---
+++ 2_homework/2_homework/Main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#pragma warning(disable : 4996)
+#include <omp.h>
+
+using namespace std;
+using std::string;
+using std::fstream;
+
+//Creating a structure for the wav file header
+struct  WAV_HEADER
+{
+    char chunkId[4];
+    unsigned long chunkSize;
+    char format[4];
+    char subchunk1Id[4];
+    unsigned long subchunk1Size;
+    unsigned short audioFormat;
+    unsigned short numChannels;
+    unsigned long sampleRate;
+    unsigned long byteRate;
+    unsigned short blockAlign;
+    unsigned short bitsPerSample;
+    char subchunk2Id[4];
+    unsigned long subchunk2Size;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    //Define number of threads from task: 4 + (514+24) % 5 = 4 + 3 = 7
+    const int g_nNumberOfThreads = 7;
+    int th_id;
+
+    WAV_HEADER Header;
+    FILE* File;
+
+    File = fopen("C:/Users/1642645/source/repos/2_homework/2_homework/sample.wav", "rb");
+
+    //Reading the file header
+    fread(&Header, sizeof(WAV_HEADER), 1, File);
+
+    //Counting the number and dimension of samples
+    int sample_size = Header.bitsPerSample / 8;
+    int samples_count = Header.subchunk2Size * 8 / Header.bitsPerSample;
+
+    //Creating an array for writing a file
+    int p_count = 0, n_count = 0;
+    short int* Data = new short int[samples_count];
+
+    omp_set_num_threads(g_nNumberOfThreads);
+
+    //Launch parallel block
+    #pragma omp parallel private (th_id)
+    {
+        th_id = omp_get_thread_num();
+
+        //Define method from task: (514+24) % 4 = 2 -> method C
+        for (int i = (((th_id + 1) * samples_count) / g_nNumberOfThreads) - 1; i >= (th_id * samples_count) / g_nNumberOfThreads; i--)
+        {
+            fread(&Data[i], sample_size, 1, File);
+        }
+        #pragma omp barrier 
+    }
+
+    //Output of the first 50 values to compare correctness with Cool Edit
+    for (int i = 0; i < 50; i++)
+    {
+        cout << i << " element: " << Data[i] << "\n";
+    }
+
+    fclose(File);
+    (void)getchar();
+    return 0;
+}
